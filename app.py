d9c044f9dd5f6b1c1f2df1c8553d03157765050f
Alec Gunny <amacgunny@gmail.com>
2019-05-29 08:43:57 -0700

    updated round id and results methods

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 from flask import Flask, request, make_response
-from event import League
+from league import League
 
 app = Flask(__name__)
 
-league = League(2019)
-league.create_event('rip-curl-pro-bells-beach')
+leagues = {}
+for year in range(2019, 2020):
+  league = League(year)
+  league.create_all_events()
+  leagues[year] = league
 
-leagues = {2019: league}
 
 @app.route('/update')
-def get_event_info():
+def get_event_results():
   event_name = request.args.get('event-name')
   year = int(request.args.get('year'))
 
-  event = leagues[year].get_event(event_name)
+  league = leagues[year]
+  if event_name not in league.events_info:
+    league.update_events_info()
+  assert event_name in league.events_info
+
+  event = league.get_event(event_name)
   results_csv = event.athlete_results_csv
   output = make_response(results_csv)
   output.headers["Content-Disposition"] = "attachment; filename=export.csv"
