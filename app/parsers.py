134bb601cee6d12c5d953e9b8e7b11ac62c229ad
Alec Gunny <amacgunny@gmail.com>
2020-01-12 14:10:47 -0800

    updates

--- app/parsers.py
+++ app/parsers.py
@@ -7,6 +7,25 @@ class EventNotReady(Exception):
 
 
 def get_event_ids(season_url, event_names=None):
+  '''
+  Finds the WSL ids assigned to the given `event_names` for the season hosted
+  at `season_url`
+  Parameters
+  -------------------------
+  season_url: str
+    a valid WSL website URL corresponding to the main page for a Men's Championship
+    Tour season
+  event_names: str or array_like(str) or None
+    either a single string event name or an iterable of string event names for
+    which to find ids. Valid events are those with links to event pages. If left
+    as None, will find ids for all currently valid events in a season. Otherwise,
+    if any specified events are not valid, a `ValueError` will be raised.
+
+  Returns
+  -------------------------
+  event_ids: dict
+    dictionary mapping event names to WSL assigned ids
+  '''
   if isinstance(event_names, str):
     event_names = [event_names]
 
@@ -22,11 +41,15 @@ def get_event_ids(season_url, event_names=None):
     event_id, event_name = event_url.split("/")[-2:]
     if event_names is None or event_name in event_names:
       event_ids[event_name] = event_id
+
+  # if event names are specifically requested and they couldn't be found,
+  # raise an error
   if (event_names is not None and
       any([name not in event_ids for name in event_names])):
     missing_names = [name for name in event_names if name not in event_ids]
     raise ValueError('Could not find events {}'.format(
       ', '.join(missing_names)))
+
   return event_ids
 
 
--- app/routes.py
+++ app/routes.py
@@ -9,8 +9,16 @@ def get_event_results():
   event_name = request.args.get('event-name')
   event_year = request.args.get('event-year')
 
-  season = wsl.Season.query.filter_by(year=event_year).first()
-  event = wsl.Event.query.filter_by(name=event_name, season_id=season.id).first()
+  season = wsl.Season.query.filter_by(year=event_year)
+  if season is None:
+    # insert code to try to create season
+    raise ValueError('Unknown season year {}'.format(event_year)
+
+  event = wsl.Event.query.filter_by(name=event_name, season_id=season.first().id)
+  if event is None:
+    # insert code to try to create event
+    raise ValueError('Event name {} unrecognized'.format(event_name))
+  event = event.first()
 
   if not event.completed:
     event.update()
