57761c4f8ead343ab59f5a4206aea87735ee62c6
Alec Gunny <alec.gunny@gmail.com>
2021-03-28 12:37:40 -0700

    adding pre-commit hooks

--- kook-tracker/kook-tracker.py
+++ kook-tracker/kook-tracker.py
@@ -1,14 +1,15 @@
 from app import app, db
 from app.models import wsl
 
+
 @app.shell_context_processor
 def make_shell_context():
-  return {
-    'db': db,
-    'Season': wsl.Season,
-    'Event': wsl.Event,
-    'Round': wsl.Round,
-    'Heat': wsl.Heat,
-    'Athlete': wsl.Athlete,
-    'HeatResult': wsl.HeatResult
-  }
+    return {
+        "db": db,
+        "Season": wsl.Season,
+        "Event": wsl.Event,
+        "Round": wsl.Round,
+        "Heat": wsl.Heat,
+        "Athlete": wsl.Athlete,
+        "HeatResult": wsl.HeatResult,
+    }
