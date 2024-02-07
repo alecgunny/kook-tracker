81be1240ca9d074f0046dc8872b88702a6dcfe88
Alec Gunny <alec.gunny@gmail.com>
2024-02-06 21:04:54 -0800

    adding app context

--- kook-tracker/app/__init__.py
+++ kook-tracker/app/__init__.py
@@ -28,29 +28,32 @@ from app import models, parsers, routes  # noqa
 # make this season if it doesn't already exist
 now = datetime.now()
 year = now.year
-season = models.wsl.Season.query.filter_by(year=year).first()
-if season is None:
-    app.logger.info(f"Initializing {year} season")
-    season = models.wsl.Season.create(year=year)
-else:
-    season_url = parsers.get_season_url(season)
-    event_metadata = parsers.get_event_data_from_season_homepage(season_url)
-    for metadata in event_metadata:
-        if metadata["status"] in ("standby", "live"):
-            id, event_name = metadata["link"].split("/")[-3:-1]
-            event = models.wsl.Event.query.filter_by(id=id).first()
-            if event is None:
-                app.logger.info(f"Initializing event {event_name}")
-                stat_id = metadata["id"]
-                event = models.wsl.Event.create(
-                    name=event_name, id=id, stat_id=stat_id, year=year
-                )
-                season.events.append(event)
-                db.session.add(event)
-
-            if not event.completed:
-                event.update()
-            db.session.commit()
-            break
+with app.app_context():
+    season = models.wsl.Season.query.filter_by(year=year).first()
+    if season is None:
+        app.logger.info(f"Initializing {year} season")
+        season = models.wsl.Season.create(year=year)
     else:
-        app.logger.info("No events to create or update")
+        season_url = parsers.get_season_url(season)
+        event_metadata = parsers.get_event_data_from_season_homepage(
+            season_url
+        )
+        for metadata in event_metadata:
+            if metadata["status"] in ("standby", "live"):
+                id, event_name = metadata["link"].split("/")[-3:-1]
+                event = models.wsl.Event.query.filter_by(id=id).first()
+                if event is None:
+                    app.logger.info(f"Initializing event {event_name}")
+                    stat_id = metadata["id"]
+                    event = models.wsl.Event.create(
+                        name=event_name, id=id, stat_id=stat_id, year=year
+                    )
+                    season.events.append(event)
+                    db.session.add(event)
+
+                if not event.completed:
+                    event.update()
+                db.session.commit()
+                break
+        else:
+            app.logger.info("No events to create or update")
